1491d7d2bfbe1191461d701fc82c8aa754cd312b
Micah Cooper <micah+github@mrmicahcooper.com>
2017-08-17 08:02:14 -0500

    Use module variables for vim.install

--- lib/tasks/dev/install.ex
+++ lib/tasks/dev/install.ex
@@ -2,16 +2,24 @@ defmodule Mix.Tasks.Dev.Install do
   use Mix.Task
   require Logger
 
+  @shortdoc """
+    Symlink prefined .dotfiles
+  """
+
+  @moduledoc """
+    Symlink prefined .dotfiles
+  """
+
   @home System.user_home()
+  @dotfiles_path File.cwd!() |> Path.join("dotfiles")
 
   def run(_args) do
-    Logger.info("sym-linking dotfiles")
-    files = File.ls!("dotfiles")
+    files = File.ls!(@dotfiles_path)
     for "." <> _ = file <- files, do: sym_link(file)
   end
 
   defp sym_link(filename) do
-    target = File.cwd!() |> Path.join("dotfiles") |> Path.join(filename)
+    target = Path.join(@dotfiles_path, filename)
     link   = Path.join(@home, filename)
     case File.ln_s(target, link) do
       :ok -> Logger.info("$ ln -s #{target} #{link}")

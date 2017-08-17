d08397221d00da57877d5e504e6c5053c51616f0
Micah Cooper <micah+github@mrmicahcooper.com>
2017-08-17 07:37:55 -0500

    Install vim bundles from ~/.vimbundle

--- lib/tasks/dev/vim.ex
+++ lib/tasks/dev/vim.ex
@@ -3,23 +3,29 @@ defmodule Mix.Tasks.Dev.Vim do
   require Logger
   import Logger, only: [info: 1]
 
-  @vim_bundle "#{System.user_home()}/.vim/bundle"
+  @shortdoc """
+    Install and update all vim plugins specified in ~/.vimbundle
+  """
 
-  @plugins File.cwd!()
-  |> Path.join(".vimbundle")
-  |> File.read!()
-  |> String.trim()
-  |> String.split("\n")
+  @vim_bundle "#{System.user_home()}/.vim/bundle"
 
   def run(_args) do
     unless File.exists?(@vim_bundle), do: File.mkdir(@vim_bundle)
 
-    @plugins
+    plugins()
     |> Enum.map(&install_or_update/1)
     |> Enum.each(&Task.await/1)
   end
 
-  def install_or_update(plugin) do
+  defp plugins do
+    System.user_home()
+    |> Path.join(".vimbundle")
+    |> File.read!()
+    |> String.trim()
+    |> String.split("\n")
+  end
+
+  defp install_or_update(plugin) do
     path = plugin_path(plugin)
     url  = plugin_url(plugin)
 
@@ -28,14 +34,14 @@ defmodule Mix.Tasks.Dev.Vim do
     end)
   end
 
-  def install(url, path) do
+  defp install(url, path) do
     result = System.cmd( "git", ["clone", url, path], [stderr_to_stdout: true])
              |> elem(0)
 
     info("\n #{result}")
   end
 
-  def update(path) do
+  defp update(path) do
     result = System.cmd("git", ["pull", "--rebase", "origin", "master"],
              [cd: path,  stderr_to_stdout: true ])
              |> elem(0)
